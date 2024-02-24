2d2a58a13bd7441b989df3b32b9d590f39a291bb
vladislav-vtr <vlad.verkhoturtsev@gmail.com>
2024-02-23 20:29:41 -0600

    first commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
+from flask_sqlalchemy import create_engine, ForeignKey
+from flask_sqlalchemy import Column, Date, Integer, String
 from flask_sqlalchemy import SQLAlchemy
 
- 
+db = SQLAlchemy()
+
+class User(db.Model):
+    username = db.Column(db.String, primary_key=True, nullable=False)
+    password = db.Column(db.String)
+
+
+
+    
+
+
+
+
+
+
